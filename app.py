68b4393af754a870c99d168f1ff4c146a487c57c
Sajjad <vosoughsajjad@gmail.com>
2025-01-08 12:49:37 +0330

    dockerize db and app

--- app.py
+++ app.py
@@ -1,43 +1,72 @@
-from flask import Flask, render_template, request, redirect, url_for
-
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask_sqlalchemy import SQLAlchemy
+import os
 app = Flask(__name__, template_folder='template')
 
-todos = [{'task': 'Sample Todo', 'done': False}]
+# Database configuration
 
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
+    'SQLALCHEMY_DATABASE_URI', 'postgresql://postgres:root@localhost/todolist'
+)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# Database model
+class ToDo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.String(255), nullable=False)
+    done = db.Column(db.Boolean, default=False)
+
+    def __repr__(self):
+        return f'<ToDo {self.task}>'
+
+# Create database tables
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def index():
+    todos = ToDo.query.all()
     return render_template('index.html', todos=todos)
-
 
 @app.route('/add', methods=['POST'])
 def add():
-    todo = request.form['todo']
-    todos.append({'task': todo, 'done':False})
+    todo_text = request.form['todo']
+    new_todo = ToDo(task=todo_text, done=False)
+    db.session.add(new_todo)
+    db.session.commit()
     return redirect(url_for('index'))
 
-
-@app.route('/edit/<int:index>', methods=["GET", "POST"])
-def edit(index):
-    todo = todos[index]
+@app.route('/edit/<int:id>', methods=["GET", "POST"])
+def edit(id):
+    todo = ToDo.query.get_or_404(id)
     if request.method == 'POST':
-        todo['task'] = request.form['todo']
+        todo.task = request.form['todo']
+        db.session.commit()
         return redirect(url_for('index'))
     else:
-        return render_template('edit.html', todo = todo, index = index)
+        return render_template('edit.html', todo=todo)
 
-@app.route('/check/<int:index>')
-def check(index):
-    todos[index]['done'] = not todos[index]['done']
+@app.route('/check/<int:id>')
+def check(id):
+    todo = ToDo.query.get_or_404(id)
+    todo.done = not todo.done
+    db.session.commit()
     return redirect(url_for('index'))
 
-
-@app.route('/delete/<int:index>')
-def delete(index):
-    del todos[index]
+@app.route('/delete/<int:id>')
+def delete(id):
+    todo = ToDo.query.get_or_404(id)
+    db.session.delete(todo)
+    db.session.commit()
     return redirect(url_for('index'))
-
-
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5000)
+
+
+
+
+
+
